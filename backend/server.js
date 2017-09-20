ee25d5e9e7a6cce73afb5b746b3db39f32625069
Matt Dennie <mattsdni@gmail.com>
2017-09-19 22:03:21 -0700

    simplified backend and added fire route

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
-import router from './router';
+import bodyParser from 'body-parser'
+import User from './models/user';
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost/users');
@@ -11,8 +12,20 @@ const app = express();
 
 // Logger that outputs all requests into the console
 app.use(morgan('combined'));
-// Use v1 as prefix for all API endpoints
-app.use('/v1', router);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/users', function(req, res) {
+  // Find all users and return json response
+  User.find().lean().exec((err, users) => res.json(
+    { users: users.map(user => ({...user, }))}
+  ));
+});
+
+app.post('/fireUser', function(req, res) {
+  // Fire the user once
+  User.findOneAndUpdate({'username': req.body.username}, {$inc : {'firedCount' : 1}}).exec((err, user) => res.json(user));
+});
 
 const server = app.listen(3000, () => {
   const { address, port } = server.address();
